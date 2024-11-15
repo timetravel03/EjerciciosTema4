aaf6a36de9b7e1c40a6e5b664c28e4c1230ed14d
telmo <telmoiglesiasfernandez@gmail.com>
2024-11-15 18:40:27 +0100

    se me colo un error

--- Ejercicio5/Form1.Designer.cs
+++ Ejercicio5/Form1.Designer.cs
@@ -51,7 +51,6 @@
             this.listBox1.Size = new System.Drawing.Size(120, 95);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
-            this.listBox1.Leave += new System.EventHandler(this.listBox1_Leave);
             // 
             // listBox2
             // 
